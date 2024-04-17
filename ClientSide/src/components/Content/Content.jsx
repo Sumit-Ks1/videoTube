4e6416ec976e9e65436df29955fa67e7061e9b5a
= <devyanshunegi@gmail.com>
2024-04-17 20:57:34 +0530

    increase responsiveness and non-scrolling of the sidebar

--- ClientSide/src/components/Content/Content.jsx
+++ ClientSide/src/components/Content/Content.jsx
@@ -3,21 +3,30 @@ import HomeDetails from '../../Details/HomeDetails'
 import VideoCard from './VideoCard/VideoCard'
 import Details from '../../Details/HomeDetails'
 import './Content.css'
+import {useState} from 'react'
 
 function Content(props) {
 
     var detail = Details[0]
     console.log(detail)
 
-    var screenWidth = window.screen.width;
+    var iWindowWidth = window.screen.width;
+    const [windowWidth, setwindowWidth] = useState(iWindowWidth)
+
+    // Function to handle the resize event
+    function handleResize() {
+        setwindowWidth(window.innerWidth)
+    }
+
+    window.addEventListener('resize', handleResize);
 
     return (
         <>
         {/* if side active add class side-active */}
             {/* <div className="side-active row row-cols-1 row-cols-lg-3 row-cols-md-2 row-cols-sm-2 g-4"> */}
-                <div className={`row row-cols-1 row-cols-lg-3 row-cols-md-2 row-cols-sm-2 g-4
-                ${props.isSideActive ? 'side-active' : ''}
-                ${screenWidth }>}`} 
+                <div className={`row row-cols-1 row-cols-xl-4 row-cols-lg-3 row-cols-md-2 row-cols-sm-2 g-4
+                ${(props.isSideActive) ? 'side-active' : ''}
+                ${windowWidth>768 ? 'side-active' : '' }`} 
                 >
                 {Details.map( (detail) => {
                     return (
