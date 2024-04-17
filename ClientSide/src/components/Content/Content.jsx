7dd50dce6ad0e2089a2790c86df502f9464dfafa
Devyanshu Negi <devyanshunegi@gmail.com>
2024-04-17 10:35:15 +0000

    pending

--- ClientSide/src/components/Content/Content.jsx
+++ ClientSide/src/components/Content/Content.jsx
@@ -4,14 +4,21 @@ import VideoCard from './VideoCard/VideoCard'
 import Details from '../../Details/HomeDetails'
 import './Content.css'
 
-function Content() {
+function Content(props) {
 
     var detail = Details[0]
     console.log(detail)
 
+    var screenWidth = window.screen.width;
+
     return (
         <>
-            <div className="side-active row row-cols-1 row-cols-lg-3 row-cols-md-2 row-cols-sm-2 g-4">
+        {/* if side active add class side-active */}
+            {/* <div className="side-active row row-cols-1 row-cols-lg-3 row-cols-md-2 row-cols-sm-2 g-4"> */}
+                <div className={`row row-cols-1 row-cols-lg-3 row-cols-md-2 row-cols-sm-2 g-4
+                ${props.isSideActive ? 'side-active' : ''}
+                ${screenWidth }>}`} 
+                >
                 {Details.map( (detail) => {
                     return (
                         <VideoCard
